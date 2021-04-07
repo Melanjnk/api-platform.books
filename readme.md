4d42b7cb2961c0f9907114b8413a67997bb4919c
Melanjnk <melanj.ks.ua@gmail.com>
2021-04-08 02:06:46 +0300

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+# TDD + API-PLATFORM
+
+### Quick Start
+
+```docker
+# Run application
+docker-composer up -d
+# Composer install
+docker-composer exec app composer install
+# Clear cache
+docker-composer exec app php bin/console c:c
+```
---
+++ tests/Controller/BookControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Controller\Tests;
+
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Entity\Book;
+
+class BookControllerTest extends KernelTestCase
+{
+    protected ?EntityManagerInterface $entityManager;
+
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+
+        DatabasePrimer::prime($kernel);
+
+        $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->entityManager->close();
+        $this->entityManager = null;
+    }
+
+    /** @test */
+    public function canBookBeCreated()
+    {
+        $book = new Book();
+        $book->setAuthor('Brandon Sanderson');
+        $book->setTitle('Mistborn');;
+        $this->entityManager->persist($book);
+
+        $this->entityManager->flush();
+
+        $bookRepository = $this->entityManager->getRepository(Book::class);
+        /** @var Book $bookRecord */
+        $bookRecord = $bookRepository->findOneBy(['title' => 'Mistborn']);
+
+        $this->assertEquals('Brandon Sanderson', $bookRecord->getAuthor());
+        $this->assertEquals('2006', $bookRecord->getPublicationDate()->format('Y'));
+        $this->assertEquals('Mistborn', $bookRecord->getTitle());
+    }
+}
